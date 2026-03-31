543ae83b279a00fc4e4dcdb50fbb09c28b5d9256
JadeFish <high5132@outlook.com>
2026-03-31 11:45:05 +0800

    oatcheck: Add list_bcp_classes.py to list host paths of BCP jars
    This script outputs absolute paths of all dexified BootClassPath jars on the host build machine.
    It uses ANDROID_PRODUCT_OUT environment variable to detect the build output path, and prompts
    users to source envsetup.sh and lunch target if the environment variable is not present.
    
    Change-Id: I90543e1c62906f3eb82694bc448ac110408cbc90

---
+++ oatcheck/list_bcp_classes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Script to list absolute paths of all dexified BootClassPath jars on host machine
+"""
+
+import os
+
+# BootClassPath from lynx device
+BOOTCLASSPATH = "/apex/com.android.art/javalib/core-oj.jar:/apex/com.android.art/javalib/core-libart.jar:/apex/com.android.art/javalib/okhttp.jar:/apex/com.android.art/javalib/bouncycastle.jar:/apex/com.android.art/javalib/apache-xml.jar:/system/framework/framework.jar:/system/framework/framework-graphics.jar:/system/framework/framework-location.jar:/system/framework/ext.jar:/system/framework/telephony-common.jar:/system/framework/voip-common.jar:/system/framework/ims-common.jar:/apex/com.android.i18n/javalib/core-icu4j.jar:/apex/com.android.adservices/javalib/framework-adservices.jar:/apex/com.android.adservices/javalib/framework-sdksandbox.jar:/apex/com.android.appsearch/javalib/framework-appsearch.jar:/apex/com.android.btservices/javalib/framework-bluetooth.jar:/apex/com.android.configinfrastructure/javalib/framework-configinfrastructure.jar:/apex/com.android.conscrypt/javalib/conscrypt.jar:/apex/com.android.crashrecovery/javalib/framework-crashrecovery.jar:/apex/com.android.devicelock/javalib/framework-devicelock.jar:/apex/com.android.healthfitness/javalib/framework-healthfitness.jar:/apex/com.android.ipsec/javalib/android.net.ipsec.ike.jar:/apex/com.android.media/javalib/updatable-media.jar:/apex/com.android.mediaprovider/javalib/framework-mediaprovider.jar:/apex/com.android.mediaprovider/javalib/framework-pdf.jar:/apex/com.android.mediaprovider/javalib/framework-pdf-v.jar:/apex/com.android.nfcservices/javalib/framework-nfc.jar:/apex/com.android.ondevicepersonalization/javalib/framework-ondevicepersonalization.jar:/apex/com.android.os.statsd/javalib/framework-statsd.jar:/apex/com.android.permission/javalib/framework-permission.jar:/apex/com.android.permission/javalib/framework-permission-s.jar:/apex/com.android.profiling/javalib/framework-profiling.jar:/apex/com.android.scheduling/javalib/framework-scheduling.jar:/apex/com.android.sdkext/javalib/framework-sdkextensions.jar:/apex/com.android.tethering/javalib/framework-connectivity.jar:/apex/com.android.tethering/javalib/framework-connectivity-t.jar:/apex/com.android.tethering/javalib/framework-tethering.jar:/apex/com.android.uwb/javalib/framework-uwb.jar:/apex/com.android.virt/javalib/framework-virtualization.jar:/apex/com.android.wifi/javalib/framework-wifi.jar"
+
+def main():
+    # Get Android product output path from environment variable
+    host_prefix = os.environ.get('ANDROID_PRODUCT_OUT')
+    if not host_prefix:
+        print("Error: ANDROID_PRODUCT_OUT environment variable not found.")
+        print("Please run the following commands first to set up the build environment:")
+        print("  source build/envsetup.sh")
+        print("  lunch aosp_lynx-trunk_staging-userdebug")
+        exit(1)
+
+    jar_paths = BOOTCLASSPATH.split(':')
+    for jar_path in jar_paths:
+        full_path = os.path.join(host_prefix, jar_path.lstrip('/'))
+        # Optional: verify file exists
+        if os.path.exists(full_path):
+            print(full_path)
+        else:
+            print(f"# WARNING: {full_path} does not exist")
+
+if __name__ == "__main__":
+    main()
